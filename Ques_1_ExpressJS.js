72407018f3700c4a4769c6889072c3ca35efad29
iShahzaib <mohd.shahzaib.hussain@iic.ac.in>
2019-10-10 14:56:05 +0530

    ExpressJS Question

---
+++ Ques_1_ExpressJS.js
@@ -0,0 +1,33 @@
+var express = require('express');  
+var app = express(); 
+var bodyParser = require('body-parser');  
+// Create application/x-www-form-urlencoded parser  
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('public'));
+
+let obj = {};
+
+app.post('/register', urlencodedParser, function (req, res) {  
+   // Prepare output in JSON format  
+    response = {  
+       username : req.body.user_name,
+       password : req.body.password,
+       confirmpassword : req.body.confirm_password,
+       email : req.body.email_id,
+       firstname : req.body.first_name,
+       lastname : req.body.last_name,
+
+   }; 
+   obj = JSON.stringify(response)
+   console.log(response);  
+   res.send("Account has been created");
+})
+var server = app.listen(8000, function () {  
+  
+  var host = server.address().address  
+  var port = server.address().port  
+  console.log("Example app listening at http://%s:%s", host, port)  
+  
+})  
+module.exports = {obj};
