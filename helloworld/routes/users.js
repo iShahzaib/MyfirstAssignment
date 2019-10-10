84fce6200b442aee4c2e00b790d77963871cb0c2
iShahzaib <mohd.shahzaib.hussain@iic.ac.in>
2019-10-10 16:03:18 +0530

    Added Express-Generator

--- helloworld/routes/users.js
+++ helloworld/routes/users.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+       // Prepare output in JSON format  
+       response = {  
+        username : req.body.user_name,
+        password : req.body.password,
+        confirmpassword : req.body.confirm_password,
+        email : req.body.email_id,
+        firstname : req.body.first_name,
+        lastname : req.body.last_name,
+ 
+    }; 
+    obj = JSON.stringify(response)
+    console.log(obj);
+
+ res.send('Account has been created');
 });
 
 module.exports = router;
